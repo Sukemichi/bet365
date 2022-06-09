2403d2ebcb4b7edc7e85e7bae3d4d548082cff4f
Sukemichi <y.sukemichi@gmail.com>
2022-06-09 16:15:03 +0900

    あ

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ document.getElementsByTagName('head')[0].appendChild(script);
 
 setTimeout (function() {
   createFilter();
-  createFavorite();
   $("div.ipn-Competition").click();
   const form = document.getElementById("form");
   const input = document.getElementById("input");
@@ -38,9 +37,6 @@ setInterval(() => {
 function createFilter() {
   $('.ipn-Classification:first-child').before('<div class="selection" style="position:relative;padding:8px 20px;color: #c3c3c3;border-bottom: solid 2px #137A5A;"><div class="ipn-CompetitionButton_TextContainer" style="font-size:14px;"><div class="ipn-CompetitionButton_Text">Filter</div></div><div class="ipn-CompetitionContainer" id="selection" style="display:flex;margin-top:8px;font-size:12px;"><div class="container"><form id="form"><input type="text" id="input" class="form-control" placeholder="不要文字列(半角英数字)" autocomplete="off" pattern="^[0-9a-zA-Z,]+$"></form><ul id="ul"></ul></div></div></div>');
 }
-function createFavorite() {
-  $('.ipn-ClassificationContainer').before('<div class="selection" style="order:1;color:#c3c3c3;border-bottom:1px solid #474747;"><div style="display:flex;align-items: center;height:45px;padding:0 20px;border-bottom:1px solid #474747;"><div>Favorite</div></div><div id="favorite"></div></div>');
-}
 
 function selection() {
   const filterList = JSON.parse(localStorage.getItem("filterWord"));
@@ -50,24 +46,6 @@ function selection() {
     })
   };
   $("div.ipn-Classification:not(:contains(Soccer))").hide();
-  const competitionButtonText = document.querySelectorAll(".ipn-CompetitionButton_Text");
-  competitionButtonText.forEach(text => {
-    if(text.textContent == ""){
-      text.parentNode.parentNode.remove();
-    }
-  });
-  const fixture = document.querySelectorAll(".ipn-Fixture");
-  fixture.forEach(item => {
-    item.oncontextmenu = function(event) {
-      event.preventDefault();
-      $(this).appendTo('#favorite').addClass("apended");
-      removeCompCont();
-      $(".apended").on("contextmenu", function(event) {
-        event.preventDefault();
-        $(this).remove();
-      });
-    };
-  });
 }
 
 function add(filter) {
@@ -96,14 +74,6 @@ function add(filter) {
   saveDate();
   input.value = "";
 };
-function removeCompCont() {
-  const competitionContainer = document.querySelectorAll(".ipn-CompetitionContainer");
-  competitionContainer.forEach(comp => {
-    if(comp.childElementCount == 0) {
-      comp.parentNode.remove();
-    }
-  });
-}
 
 function saveDate() {
   const lists = document.querySelectorAll("li");
